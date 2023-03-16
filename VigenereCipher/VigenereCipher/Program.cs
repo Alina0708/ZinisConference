7e6db5e5048238acadc304a7062c11e76f69aff8
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-03-16 18:07:27 +0300

    namespaces renamed

--- VigenereCipher/VigenereCipher/Program.cs
+++ VigenereCipher/VigenereCipher/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace VigenereCipher
+namespace Vigenere
 {
 	public class VigenereCipher
 	{
--- VigenereCipher/VigenereWithTwoKeys/Program.cs
+++ VigenereCipher/VigenereWithTwoKeys/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace VigenereWithTwoKeys
+﻿namespace Vigenere
 {
 	internal class Program
 	{
